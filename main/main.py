a594bcd8a28536bef1c80f9d64c69f67385dcd8d
devangshrivastava <devangshrivastava27@gmail.com>
2024-02-16 17:39:27 +0530

    made seed and peer properly

--- main/main.py
+++ main/main.py
@@ -1,7 +1,36 @@
-from seed import start_seed
+from seed.devang_seed import Seed
+from peer.peer import Peer 
+import time
+
+try:
+    seed1 = Seed("127.0.0.1", 7000)
+    seed1.start()
+    seed2 = Seed("127.0.0.1", 7001)
+    seed2.start()
+
+    peer1 = Peer("127.0.0.1", 8000)
+    peer1.start()
+    peer2 = Peer("127.0.0.1", 8001)
+    peer2.start()
+    peer3 = Peer("127.0.0.1", 8002)
+    peer3.start()
+    peer4 = Peer("127.0.0.1", 8003)
+    peer4.start()
+
+    time.sleep(2)
+    peer1.connect(seed1.host, seed1.port)
+    time.sleep(1)
+    peer2.connect(seed1.host, seed1.port)
+    time.sleep(1)
+    peer3.connect(seed1.host, seed1.port)
+    time.sleep(1)
+    peer4.connect(seed1.host, seed1.port)
+    time.sleep(1)
+    print(peer3.peers)
+    print("done")
 
 
-seed_nodes = start_seed.start_seed()
-for node in seed_nodes:
-    print(node.soc)
+except KeyboardInterrupt:
+    print("Shutting down...")
+    print("Goodbye!")
 
--- main/peer/main_peer.py
+++ main/peer/main_peer.py
@@ -1,69 +1,44 @@
-
 from peer import Peer
-import signal
-import sys
 import time
-import random
 
-# Create signal handler function
-def signal_handler(sig, frame):
-    print('Exiting...')
-    # node1.close_connections()
-    # node2.close_connections()
-    sys.exit(0)
-
-# Register signal handler for SIGINT (Ctrl+C)
-signal.signal(signal.SIGINT, signal_handler)
-
-# Create two peers
 try:
 
-    given_node = Peer("0.0.0.0", 7000)
+    given_node = Peer("127.0.0.1", 7000)
     given_node.start()
     nodes = []
     for port in range(8000, 8006):
-        node = Peer("0.0.0.0", port)
+        node = Peer("127.0.0.1", port)
         nodes.append(node)  
 
     for node in nodes:
         node.start()
     time.sleep(2) # Give some time for nodes to start listening
-
+    
+    
+    
+    node1 = nodes[0]    
+    for node in nodes:
+        node.connect(given_node.host, given_node.port)
     
 
-    # Connect to seed
-    given_node.connect(nodes)
-
-    for conn in given_node.connected_to:
+    for conn in given_node.connections:
         print(conn)
 
     input("Press Enter to send data...")
-    # while True:
-    #     time.sleep(1)
-    #     a = input("Enter node number: ")
-    #     m = input("Enter message: ")
-    #     b = input("Do you want to continue? (yes/no): ")
-    #     if b == "no":
-    #         break
-    #     elif b == "yes":
-    #         continue
-    #     else:
-    #         print("Invalid input")
-    #         break
         
     
     for node in nodes:
         node.close_connections()
     given_node.close_connections()
-    # node1.close_connections()
+    node1.close_connections()
     # node2.close_connections()
-
-
-
-
-
+    
 
 except KeyboardInterrupt:
-    print('Interrupted by user. Closing connections...')
-    # node1.close_connections()
-    # node2.close_connections()
+    print("Shutting down...")
+    for node in nodes:
+        node.close_connections()
+    given_node.close_connections()
+    node1.close_connections()
+    # node2.close_connections()/
+    print("Goodbye!")
